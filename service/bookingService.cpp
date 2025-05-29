f05178d41c33f3e7afc998ed939a93fa1e401653
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-05-29 16:58:52 +0530

    working with booking service

--- service/bookingService.cpp
+++ service/bookingService.cpp
@@ -1,6 +1,33 @@
 #include "bookingService.hpp";
+#include "../entities/vehicle.hpp"
+#include "../entities/train.hpp"
+#include "../entities/user.hpp"
+#include <fileIO.cpp>
+
+
+
 
 template <typename T>
-void BookingService<T>::book(string entityId , string userId){
-      
+void BookingService<T>::book(string entityId , User user , string name , string source , string destination){
+        
+
+         struct Vehicle vehicle;
+         vehicle.vehicleId = entityId;
+         vehicle.name = name;
+         vehicle.source = source;
+         vehicle.destination = destination;
+         vehicle.time = time(NULL);
+         vehicle.seats = {{user}};
+
+
+         saveBooking(vehicle);
 }
+
+template <typename T>
+void saveBooking(T entity){
+   FileIO file;
+   file.saveToFile(entity);
+}
+
+
+
